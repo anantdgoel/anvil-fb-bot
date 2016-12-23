d1c08a870449024f70ca40ef48a06a3a5b9cafc6
anantdgoel@gmail.com <anantdgoel@gmail.com>
2016-12-23 01:14:14 -0600

    logging

--- app.py
+++ app.py
@@ -79,6 +79,29 @@ def send_message(recipient_id, message_text):
         log(r.status_code)
         log(r.text)
 
+def send_message(recipient_id, message_text, quick_replies):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
 
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
@@ -88,6 +111,7 @@ def add_appointment():
     print("Test action")
 
 def send(request, response):
+    log(str(response))
     send_message(request['session_id'], response['text'])
 
 
