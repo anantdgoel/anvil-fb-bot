cac9fff9207c3432124496a512477045d8c75200
anantdgoel@gmail.com <anantdgoel@gmail.com>
2016-12-22 21:43:08 -0600

    fixed typo

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    response = client.messsage(message_text)
+                    response = client.message(message_text)
                     send_message(sender_id, response)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
