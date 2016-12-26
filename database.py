bdb1cdbfbd0efa9f8518a43195cfa05fffbe1eff
Mihir Tiwari <mtiw999@gmail.com>
2016-12-26 17:12:49 -0500

    can access stuff stored in firebase database

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from firebase import firebase
 
-if __name__ == '__main__':
-    firebase = firebase.FirebaseApplication('https://anvilappointments.firebaseio.com', None)
-    result = firebase.get('/users', None)
-    print result
+firebase = firebase.FirebaseApplication('https://anvilappointments.firebaseio.com', None)
+result = firebase.get('/users', None)
+print result
